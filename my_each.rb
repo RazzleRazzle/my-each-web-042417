8bf4e49c2f6f9e28fe3394cb447e0d2cb9233495
Jake Musich <makejusich@gmail.com>
2017-04-13 21:42:26 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,13 @@
-def my_each # put argument(s) here
-  # code here
+#method and argument
+#enact iteration - while loop checking length of local variable=array.length and increment it until equal to the last index number of the array
+#yield with [] to grab element at [index] value above and yield that element to the block called by method
+#call method with array and block that transforms the yielded array value
+
+def my_each(array)
+  i = 0
+  while i < array.length do
+    yield array[i]
+      i += 1
+  end
+  array 
 end
